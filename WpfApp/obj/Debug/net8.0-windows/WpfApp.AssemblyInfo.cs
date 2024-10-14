033bac36ce2314cf8bf56c030dd5c986504c625f
Darya Shadrina <d.shadrina@mirtekgroup.ru>
2024-10-14 11:56:43 +0300

    Add button, background color

--- WpfApp/obj/Debug/net8.0-windows/WpfApp.AssemblyInfo.cs
+++ WpfApp/obj/Debug/net8.0-windows/WpfApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad1b4bf779f7c82e975a7f4fd0a269c0503584b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WpfApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
